4ba52b7e4a9e0dace2f450ff911ce933c2b60a10
Lee JH <56266206+syous154@users.noreply.github.com>
2025-03-04 16:50:14 +0900

    Add LeeJH_2839.cpp

---
+++ Greedy_and_BruteForce/LeeJH_2839.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int cnt1 = n / 5;
+    int cnt2 = 0;
+
+    while(1){
+        if(cnt1 < 0){
+            break;
+        }
+        if((n - ( 5 * cnt1)) % 3 == 0){
+            cnt2 = (n - (5 * cnt1)) / 3;
+            break;
+        }
+        cnt1--;
+    }
+
+    cout<<cnt1 + cnt2<<'\n';
+
+    return 0;
+}   
